d14c156244c2e30e9b25d2a73436d91496c53895
NUAA-YANG <2997357856@qq.com>
2023-06-01 17:08:34 +0800

    实现远程调用服务器

---
+++ src/main/java/com/nuaa/isisnetwork/linuxCommand/ExecLinuxCommands.java
@@ -0,0 +1,77 @@
+package com.nuaa.isisnetwork.linuxCommand;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author YZX
+ * @Create 2023-05-04 11:09
+ * @Java-version jdk1.8
+ */
+@Service
+@Component
+//执行服务器cmd命令
+public class ExecLinuxCommands {
+
+    /**
+     * @description  执行Linux命令
+     * @date 2023/5/4 14:58
+     * @params [session传递连接对话, commands传递命令集合]
+     * @returns List<String> 返回命令执行结果
+     */
+    public List<String> getCmdResult(Session session , List<String> commands) throws InterruptedException {
+        //用来存放命令的返回值
+        List<String> cmdResult = new ArrayList<>();
+        for (String command : commands) {
+            Channel channel = null;
+            try {
+                //创建执行通道
+                channel = session.openChannel("exec");
+                //设置命令
+                ((ChannelExec) channel).setCommand(command);
+                //连接通道
+                channel.connect();
+                //读取通道的输出
+                InputStream in = channel.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                //存放命令的执行结果，如果结果有很多行，则每次line的值不同
+                String line;
+                //lines用来拼接line结果
+                StringBuffer lines = new StringBuffer();
+                while ((line = reader.readLine()) != null) {
+                    //去除头尾的空格
+                    line.trim();
+                    lines = lines.append(line);
+                }
+                //如果命令执行没有返回值，则直接输出没有返回值
+                if (String.valueOf(lines).equals("")){
+                    cmdResult.add("命令执行成功,但没有返回值");
+                    //cmdResult.add("命令["+command+"]执行成功,但没有返回值");
+                }else {
+                    //否则将每行返回直接存入到list中
+                    cmdResult.add(String.valueOf(lines));
+                }
+                reader.close();
+                channel.disconnect();
+            } catch (JSchException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            //每次运行完一条命令休眠一会儿，让服务器响应
+            Thread.sleep(200);
+        }
+        return cmdResult;
+    }
+}
---
+++ src/main/java/com/nuaa/isisnetwork/linuxCommand/LinuxConnection.java
@@ -0,0 +1,53 @@
+package com.nuaa.isisnetwork.linuxCommand;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author YZX
+ * @Create 2023-05-04 11:04
+ * @Java-version jdk1.8
+ */
+@Service
+@Component
+//获取服务器的连接
+public class LinuxConnection {
+
+    /***
+     * @description  jsch创建连接
+     * @date 2023/5/4 14:56
+     * @params [ip地址, port端口号, userName用户名, password密码]
+     * @returns Session 对象
+     */
+    public Session getJSchSession(String ip , int port, String userName, String password){
+        JSch jSch = new JSch();
+        Session session = null;
+        try {
+            //创建连接
+            session = jSch.getSession(userName,ip,port);
+            session.setPassword(password);
+            //是否使用密钥登录，一般默认为no
+            session.setConfig("StrictHostKeyChecking", "no");
+            //启用连接
+            session.connect();
+            //System.out.println("======================服务器连接成功=======================");
+        }catch (Exception e){
+            throw new RuntimeException("======================服务器连接失败=======================");
+        }
+        return session;
+    }
+
+    //jsch关闭连接
+    public void closeJSchSession(Session session){
+        if (session != null) {
+            try {
+                session.disconnect();
+                System.out.println("====================服务器连接关闭成功======================");
+            }catch (Exception e){
+                throw new RuntimeException("======================服务器连接关闭失败=======================");
+            }
+        }
+    }
+}
