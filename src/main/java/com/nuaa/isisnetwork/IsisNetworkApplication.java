07d351bc3074f3c1fef569aba92dadc94a26569d
NUAA-YANG <2997357856@qq.com>
2023-06-01 16:22:32 +0800

    项目初始化

---
+++ src/main/java/com/nuaa/isisnetwork/IsisNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.nuaa.isisnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsisNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsisNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nuaa/isisnetwork/IsisNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuaa.isisnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsisNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
